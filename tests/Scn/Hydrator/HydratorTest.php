1da3f4915bd71cc6b0ee0516f29feaf20d3e16e2
Alexander Wühr <wuehr@sc-networks.com>
2018-07-24 16:25:47 +0200

    Allow hydration with lists
    (cherry picked from commit 2957c4a04e5919050bdd985f28b4093804a7368f)
    Signed-off-by: Alexander Wühr <wuehr@sc-networks.com>

--- tests/Scn/Hydrator/HydratorTest.php
+++ tests/Scn/Hydrator/HydratorTest.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace Test\Scn\Hydrator;
 
 use PHPUnit\Framework\TestCase;
+use Scn\Hydrator\Configuration\ExtractorConfigInterface;
 use Scn\Hydrator\Configuration\GenericExtractorConfig;
 use Scn\Hydrator\Configuration\GenericHydratorConfig;
+use Scn\Hydrator\Configuration\HydratorConfigInterface;
 use Scn\Hydrator\Hydrator;
 
 final class HydratorTest extends TestCase
@@ -22,26 +24,53 @@ final class HydratorTest extends TestCase
      */
     private $testValueStorage = [];
 
+    /**
+     * @var \Closure
+     */
+    private $testPropertySetter;
+
+    /**
+     * @var \Closure
+     */
+    private $testPropertyGetter;
+
     public function setUp(): void
     {
         $this->subject = new Hydrator();
         $this->testValueStorage = [];
-    }
-
-    public function testHydrate(): void
-    {
-        $testPropertySetter = function (
+        $this->testPropertySetter = function (
             $value,
             string $name
         ): void {
             $this->testValueStorage[$name] = $value;
         };
+        $this->testPropertyGetter = function (string $name) {
+            return $this->testValueStorage[$name];
+        };
+    }
 
-        $hydratorConfig = new GenericHydratorConfig([
-            'prop1' => $testPropertySetter,
-            'prop2' => $testPropertySetter,
-            'prop3' => $testPropertySetter,
-        ]);
+    private function createHydratorConfig(...$propertyNames): HydratorConfigInterface
+    {
+        $setters = array_pad([], count($propertyNames), $this->testPropertySetter);
+
+        return new GenericHydratorConfig(array_combine($propertyNames, $setters));
+    }
+
+    private function createExtractorConfig(...$propertyNames): ExtractorConfigInterface
+    {
+        $getters = array_pad([], count($propertyNames), $this->testPropertyGetter);
+
+        return new GenericExtractorConfig(array_combine($propertyNames, $getters));
+    }
+
+    private function assertHydrationResult(array $expectedData): void
+    {
+        $this->assertSame($expectedData, $this->testValueStorage);
+    }
+
+    public function testHydrate(): void
+    {
+        $hydratorConfig = $this->createHydratorConfig('prop1', 'prop2', 'prop3');
 
 
         $randomInt = random_int(0, PHP_INT_MAX);
@@ -53,23 +82,44 @@ final class HydratorTest extends TestCase
 
         $this->subject->hydrate($hydratorConfig, $this, $testData);
 
-        $this->assertSame($testData, $this->testValueStorage);
+        $this->assertHydrationResult($testData);
+    }
+
+    public function testHydrateWithIgnoreKeys(): void
+    {
+        $hydratorConfig = $this->createHydratorConfig('prop1', 'prop2', 'prop3');
+
+        $randomInt = random_int(0, PHP_INT_MAX);
+        $testData = [
+            'prop1' => 'fu',
+            'prop2' => $randomInt,
+            'prop3' => null,
+        ];
+
+        $this->subject->hydrate($hydratorConfig, $this, array_values($testData), Hydrator::IGNORE_KEYS);
+
+        $this->assertHydrationResult($testData);
     }
 
     public function testHydrateIsStrictByDefault()
     {
+        $hydratorConfig = $this->createHydratorConfig('prop1');
 
-        $testPropertySetter = function (
-            $value,
-            string $name
-        ): void {
-            $this->testValueStorage[$name] = $value;
-        };
+        $testData = [
+            'unexpected' => true,
+            'prop1' => 'fu',
+            'another_unexpected' => true,
+        ];
 
-        $hydratorConfig = new GenericHydratorConfig([
-            'prop1' => $testPropertySetter,
-        ]);
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Unexpected data: unexpected, another_unexpected');
 
+        $this->subject->hydrate($hydratorConfig, $this, $testData);
+    }
+
+    public function testHydrateOnNonStrict()
+    {
+        $hydratorConfig = $this->createHydratorConfig('prop1');
 
         $testData = [
             'unexpected' => true,
@@ -81,59 +131,47 @@ final class HydratorTest extends TestCase
             'prop1' => 'fu',
         ];
 
-        $this->expectException(\InvalidArgumentException::class);
-        $this->expectExceptionMessage('Unexpected data: unexpected, another_unexpected');
-
-        $this->subject->hydrate($hydratorConfig, $this, $testData, 0);
-
-        $this->assertSame($expectedResult, $this->testValueStorage);
-    }
-
-    public function testHydrateOnNonStrict()
-    {
-        $hydratorConfig = new GenericHydratorConfig([
-            'prop1' => 'strval',
-        ]);
-
-
-        $testData = [
-            'unexpected' => true,
-            'prop1' => 'fu',
-            'another_unexpected' => true,
-        ];
-
-        $this->expectException(\InvalidArgumentException::class);
-        $this->expectExceptionMessage('Unexpected data: unexpected, another_unexpected');
-
-        $this->subject->hydrate($hydratorConfig, $this, $testData, 0);
+        $this->subject->hydrate($hydratorConfig, $this, $testData, Hydrator::NO_STRICT_KEYS);
+        $this->assertHydrationResult($expectedResult);
     }
 
     public function testExtract(): void
     {
-        $testPropertyGetter = function (
-            string $name
-        ) {
-            return $this->testValueStorage[$name];
-        };
-
-        $extractorConfig = new GenericExtractorConfig([
-            'prop1' => $testPropertyGetter,
-            'prop2' => $testPropertyGetter,
-            'prop3' => $testPropertyGetter,
-        ]);
-
+        $extractorConfig = $this->createExtractorConfig('prop1', 'prop2', 'prop3');
 
         $randomInt = random_int(0, PHP_INT_MAX);
 
         $this->testValueStorage = [
-            'prop1' => ['fu', $this],
-            'prop2' => [$randomInt, $this],
-            'prop3' => [null, $this],
+            'prop1' => 'fu',
+            'prop2' => $randomInt,
+            'prop3' => null,
         ];
 
         $this->assertSame(
             $this->testValueStorage,
             $this->subject->extract($extractorConfig, $this)
+        );
+    }
+
+    public function testExtractionResultEqualsHydrationData()
+    {
+        $propertyNames = ['prop1', 'prop2', 'prop3'];
+
+        $hydratorConfig = $this->createHydratorConfig(...$propertyNames);
+        $extractorConfig = $this->createExtractorConfig(...$propertyNames);
+
+        $testData = array_combine($propertyNames, [
+            random_int(0, PHP_INT_MAX),
+            random_int(0, PHP_INT_MAX),
+            random_int(0, PHP_INT_MAX),
+        ]);
+
+        $this->subject->hydrate($hydratorConfig, $this, $testData);
+        $extractorResult = $this->subject->extract($extractorConfig, $this);
+
+        $this->assertSame(
+            $testData,
+            $extractorResult
         );
     }
 }
