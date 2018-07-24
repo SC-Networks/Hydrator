a204941d5164358175424523a76d5c34f71413bd
Alexander Wühr <wuehr@sc-networks.com>
2018-07-24 10:31:39 +0200

    Allow all callables for properties

--- src/Scn/Hydrator/Hydrator.php
+++ src/Scn/Hydrator/Hydrator.php
@@ -10,14 +10,19 @@ use Scn\Hydrator\Configuration\HydratorConfigInterface;
 final class Hydrator implements HydratorInterface
 {
 
+    private function invoke(callable $callback, object $entity, ...$args)
+    {
+        if ($callback instanceof \Closure) {
+            $callback = $callback->bindTo($entity, $entity);
+        }
+
+        return $callback(...$args);
+    }
+
     public function hydrate(HydratorConfigInterface $config, object $entity, array $data): void
     {
         foreach ($config->getHydratorProperties() as $propertyName => $set) {
-            if ($set instanceof \Closure) {
-                $set = $set->bindTo($entity, $entity);
-            }
-
-            $set($data[$propertyName] ?? null, $propertyName);
+            $this->invoke($set, $entity, $data[$propertyName] ?? null, $propertyName);
         }
     }
 
@@ -25,17 +30,8 @@ final class Hydrator implements HydratorInterface
     {
         $data = [];
 
-        /**
-         * @var string $propertyName
-         * @var \Closure $get
-         */
         foreach ($config->getExtractorProperties() as $propertyName => $get) {
-            if (!$get instanceof \Closure) {
-                throw new \RuntimeException('Must be closure');
-            }
-
-            $get = $get->bindTo($entity, $entity);
-            $data[$propertyName] = $get($propertyName);
+            $data[$propertyName] = $this->invoke($get, $entity, $propertyName);
         }
 
         return $data;
